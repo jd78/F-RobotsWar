322ce7ba2c32679da14b506e9420120a7f17a684
Gennaro Del Sorbo <gennarodelsorbo@icloud.com>
2015-07-19 00:10:00 +0100

    2 robot wars

--- src/RobotWar/Program.fs
+++ src/RobotWar/Program.fs
@@ -6,25 +6,39 @@ let main argv =
 
     let arena = System.Console.ReadLine() |> fun s -> Validator.Input s 2 |> fun s -> (int s.[0], int s.[1])
 
-    printfn "Please enter the initial robot position in the grid follwed by a direction (N,S,E,W) (eg 1 2 N)"
+    let setRobot() =
 
-    let robot = 
-        System.Console.ReadLine()
-        |> fun s -> Validator.Input s 3 
-        |> fun s -> Validator.GridPosition s arena
-        |> Validator.Facing 
-        |> fun r -> (int r.[0], int r.[1], Robot.StringToFacing(r.[2]))
+        printfn "Please enter the initial robot position in the grid follwed by a direction (N,S,E,W) (eg 1 2 N)"
 
-    printf "R -> rotate right, L -> rotate left, M -> move, press enter to stop and get the final position\n"
+        let robot = 
+            System.Console.ReadLine()
+            |> fun s -> Validator.Input s 3 
+            |> fun s -> Validator.GridPosition s arena
+            |> Validator.Facing 
+            |> fun r -> (int r.[0], int r.[1], Robot.StringToFacing(r.[2]))
+        robot
 
-    let rec recursiveCommand (x: int, y: int, facing: Facing) =
-        let robot = (x, y, facing)
-        System.Console.ReadKey() |> fun k ->
-            match k.Key with
-            | System.ConsoleKey.Enter -> (x, y, facing)
-            | _ -> Robot.ExecuteCommand robot arena k.KeyChar |> recursiveCommand 
+    let startGame robot = 
+        printf "R -> rotate right, L -> rotate left, M -> move, press enter to stop and get the final position\n"
 
-    recursiveCommand robot |> fun (x, y, facing) -> printf "final position: x: %d, y: %d, f: %c" x y (facing.ToString().[0])
+        let rec recursiveCommand (x: int, y: int, facing: Facing) =
+            let robot = (x, y, facing)
+            System.Console.ReadKey() |> fun k ->
+                match k.Key with
+                | System.ConsoleKey.Enter -> (x, y, facing)
+                | _ -> Robot.ExecuteCommand robot arena k.KeyChar |> recursiveCommand
+
+        recursiveCommand robot
+
+    let finalPosition (x, y, facing) =
+        printf "%d %d %c\n\n" x y (facing.ToString().[0])
+
+    let r1 = setRobot() |> startGame
+    let r2 = setRobot() |> startGame
+
+    printf "Game ended\n\n"
+    finalPosition r1
+    finalPosition r2
 
     System.Console.ReadLine() |> ignore
     0
